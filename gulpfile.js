a8179bf2530f64b5b74c1b2da0cf9efafaf463bc
Loz Lawn <Loz Lawn>
2016-02-15 22:35:51 +1100

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	contact = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources =[
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+
+	];
 
 gulp.task ('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({ bare: true })
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(contact('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
