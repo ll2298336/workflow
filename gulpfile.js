a683a75e6f8949fba7986387c63adff5cdb44e1c
Loz Lawn <Loz Lawn>
2016-02-16 18:51:42 +1100

    Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,8 @@ var jsSources =[
 
 
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task ('coffee', function() {
 	gulp.src(coffeeSources)
@@ -48,6 +50,8 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function() {
@@ -57,4 +61,14 @@ gulp.task('connect', function() {
 	});
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('html', function(){
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+	gulp.src(jsonSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'coffee', 'json', 'js', 'compass', 'connect', 'watch']);
