81ecf230c1cfb748f2e4d7727391664507531209
Loz Lawn <Loz Lawn>
2016-02-16 19:38:10 +1100

    Added Gulpif and minified production javascript

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
 	contact = require('gulp-concat'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
 	connect = require('gulp-connect');
 
 var env,
@@ -51,6 +53,7 @@ gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(contact('script.js'))
 		.pipe(browserify())
+		.pipe(gulpif(env === 'production', uglify()))
 		.pipe(gulp.dest(outputDir + 'js'))
 		.pipe(connect.reload())
 });
